fe03fed177763f324e5370d8dab35ff7a0feb97f
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-10 09:38:50 +0530

    Time: 92 ms (5.24%), Space: 20.2 MB (94.16%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i-1]==nums[i]:
+                return True
+        return False
+
+        
