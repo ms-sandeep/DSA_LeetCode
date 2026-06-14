8a245e89142b0acb5df8f5ed17734a86855d26b8
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-14 12:04:58 +0530

    Time: 0 ms (100%), Space: 13.6 MB (74.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,19 @@
 class Solution(object):
     def majorityElement(self, nums):
-        dic={}
+        nums.sort()
         temp=len(nums)//2
-        for i in nums:
-            if i in dic:
-                dic[i]+=1
-            else:
-                dic[i]=1
-        for j in dic:
-            val=dic[j]
-            if val>temp:
-                return j
+        return nums[temp]
+        # dic={}
+        # temp=len(nums)//2
+        # for i in nums:
+        #     if i in dic:
+        #         dic[i]+=1
+        #     else:
+        #         dic[i]=1
+        # for j in dic:
+        #     val=dic[j]
+        #    if val>temp:
+        #        return j
         
 
 
