a9b2b38de57a580a3f5fe104b609c433482fd5f5
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-18 22:29:41 +0530

    Time: 22 ms (42.93%), Space: 14 MB (40.73%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findErrorNums(self, nums):
+        duplicate=float('-inf')
+        missing=float('-inf')
+        s=set()
+        for i in range(len(nums)):
+            if nums[i] not in s:
+                s.add(nums[i])
+            else:
+                duplicate=nums[i]
+        for j in range(1,len(nums)+1):
+            if j not in s:
+                missing=j
+        return[duplicate, missing]
+        
