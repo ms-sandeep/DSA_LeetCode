f7cbe0cf9e6becbae857cd6a4a61b10d2b207ec1
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-19 18:37:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.77%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def plusOne(self, digits):
-        st=''
-        ls=[]
-        for i in digits:
-            st+=str(i)
-
-        st=int(st)+1
-        for j in str(st):
-            ls+=[int(j)]
-        return ls
+        cary=1
+        for i in range(len(digits)-1,-1,-1):
+            sum_val=digits[i]+cary
+            digits[i]=sum_val%10
+            cary=sum_val/10
+        if cary==1:
+            digits=[1]+digits
+        return digits
