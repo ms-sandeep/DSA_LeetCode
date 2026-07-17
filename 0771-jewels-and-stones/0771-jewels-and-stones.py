47e541d092c228caf20b97cb8d0165f4c9a00216
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-07-17 18:55:20 +0530

    Time: 0 ms (100%), Space: 12.5 MB (16.64%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,12 +1,7 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
-        dic={}
-        for i in jewels:
-            if i not in dic:
-                dic[i]=0
         count=0
-        for j in stones:
-            if j in dic:
-                dic[j]+=1
+        for i in stones:
+            if i in jewels:
                 count+=1
         return count
