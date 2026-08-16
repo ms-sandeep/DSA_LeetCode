8ff115d836c600fbf34d326c7db36c592978cf60
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-08-16 22:43:26 +0530

    Time: 1315 ms (11.69%), Space: 21.9 MB (19.12%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def sortArray(self, nums):
+        
+        def merge(nums, low, mid, high):
+            i=low
+            j=mid+1
+            lst=[]
+
+            while i<=mid and j<=high:
+                if nums[i]< nums[j]:
+                    lst.append(nums[i])
+                    i+=1
+                else:
+                    lst.append(nums[j])
+                    j+=1
+            while i<=mid:
+                lst.append(nums[i])
+                i+=1
+            
+            while j<=high:
+                lst.append(nums[j])
+                j+=1
+
+            for k in range(len(lst)):
+                nums[low+k]=lst[k]
+        def mergeSort(nums, low, high):
+            if low==high:
+                return
+            
+            mid=(low + high) // 2
+
+            mergeSort(nums, low, mid)
+            mergeSort(nums, mid+1, high)
+            merge(nums, low, mid, high)
+        mergeSort(nums, 0, len(nums)-1)
+
+        return nums
+        
+        
