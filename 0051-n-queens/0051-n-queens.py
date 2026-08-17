adc4d0774aaddac32bd5a7168b4c61e86261752a
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-08-17 21:22:45 +0530

    Time: 50 ms (12.06%), Space: 12.5 MB (91.69%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        res=[]
+        board=[["."]*n for _ in range(n)]
+
+        def isSafe(row, col):
+            for i in range(row):
+                if board[i][col]=="Q":
+                    return False
+
+            i, j = row-1, col-1
+            while i>=0 and j>=0:
+                if board[i][j]=="Q":
+                    return False
+                i-=1
+                j-=1
+
+            i,j = row-1, col+1
+            while i>=0 and j<n:
+                if board[i][j]=="Q":
+                    return False
+                i-=1
+                j+=1
+            return True   
+
+        def backtrack(row):
+            if row == n :
+                temp=["".join(r) for r in board]
+                res.append(temp)
+                return
+            for col in range(n):
+                if isSafe(row, col):
+                    board[row][col]="Q"
+                    backtrack(row+1)
+                    board[row][col]="."
+        
+        backtrack(0)
+        return res
+        
