9a7a8ec94cc294f85831c39421013fc5e08b2960
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-08-22 18:58:54 +0530

    Time: 0 ms (100%), Space: 12.3 MB (56.2%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def checkDivisibility(self, n):
-        sum = 0
+        total = 0
         product = 1
         temp = n
 
-        while temp>0:
-            rem = temp% 10
+        while temp > 0:
+            rem = temp % 10
             temp //= 10
-            sum += rem
-            product*= rem
+            total += rem
+            product *= rem
 
-        return n % (sum + product) == 0
+        return n % (total + product) == 0
 
 
         
